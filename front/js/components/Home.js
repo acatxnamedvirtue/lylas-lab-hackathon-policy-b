0d74df13a31a33bd0cdf1fcef6100ce20de18153
acatxnamedvirtue <tyler@meetup.com>
2018-10-20 11:35:32 -0400

    update home

--- front/js/components/Home.js
+++ front/js/components/Home.js
@@ -11,45 +11,52 @@ class Home extends React.Component {
 
   componentDidMount() {}
 
-  static renderHomeView() {
+  renderHomeView() {
     return (
       <div id="home-container">
         <h1>What are you looking for?</h1>
 
-        <div id="insurance-button" className="button">
+        <div
+          id="insurance-button"
+          className="button"
+          onClick={() => this.setState({ activeView: 'INSURANCE' })}
+        >
           Insurance
         </div>
-        <div id="pharmacy-button" className="button">
-          Pharmacy );
+        <div
+          id="pharmacy-button"
+          className="button"
+          onClick={() => this.setState({ activeView: 'PHARMACY' })}
+        >
+          Pharmacy
         </div>
       </div>
     );
   }
 
-  static renderInsuranceView() {
+  renderInsuranceView() {
     return undefined;
   }
 
-  static renderPharmacyView() {
+  renderPharmacyView() {
     return undefined;
   }
 
-  static renderActiveView(view) {
+  renderActiveView(view) {
     switch (view) {
-      case 'HOME':
-        return Home.renderHomeView();
-      case 'INSURANCE':
-        return Home.renderInsuranceView();
-      case 'PHARMACY':
-        return Home.renderPharmacyView();
       default:
-        return Home.renderHomeView();
+      case 'HOME':
+        return this.renderHomeView();
+      case 'INSURANCE':
+        return this.renderInsuranceView();
+      case 'PHARMACY':
+        return this.renderPharmacyView();
     }
   }
 
   render() {
     const { activeView } = this.state;
-    return <div id="app-container">{Home.renderActiveView(activeView)}</div>;
+    return <div id="app-container">{this.renderActiveView(activeView)}</div>;
   }
 }
 
