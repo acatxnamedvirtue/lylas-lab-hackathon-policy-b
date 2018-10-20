96cbe9aa1dfb06745258fc897f0ac0d26552a569
nikymorg <nicole.morgan@gmail.com>
2018-10-20 14:44:47 -0400

    reformat plan data

--- app/adapters/vericred_adapter.rb
+++ app/adapters/vericred_adapter.rb
@@ -10,41 +10,54 @@ class VericredAdapter
   end
 
   def get_plans(state)
-    drug_ids = ENV['drug_ids'].split(", ").first(10)
-    coverages = drug_ids.map{|drug_id| get_coverages(drug_id, state)}.flatten.first(10)
-    coverages.map{|coverage| get_plan(coverage["plan_id"])}
+    drug_ids = ENV['drug_ids'].split(', ').first(10)
+    coverages = drug_ids.map{|drug_id| get_coverages(drug_id, state)}.flatten.shuffle.first(10)
+    coverages.map{|coverage| get_plan(coverage)}
   end
 
   def get_coverages(drug_id, state)
     url = base_url + "drug_packages/#{drug_id}/coverages?audience=individual&state_code=#{state}"
     resp = RestClient.get(url, headers)
-    data = JSON.parse(resp)
-    data["drug_coverages"]
+    JSON.parse(resp)['drug_coverages']
   end
 
-  def get_plan(plan_id)
+  def get_plan(coverage)
     year = DateTime.now.year
-    url = base_url + "plans/medical/#{plan_id}?year=#{year}"
+    url = base_url + "plans/medical/#{coverage['plan_id']}?year=#{year}"
     resp = RestClient.get(url, headers){|response, request, result| response }
     plan = JSON.parse(resp)['plan']
-    format_plan(plan)
+    format_plan(plan, coverage['tier'])
   end
 
-  def format_plan(plan)
-    plan.slice(
-      "carrier_name",
-      "display_name",
-      "plan_type",
-      "family_drug_deductible",
-      "individual_drug_deductible",
-      "generic_drugs",
-      "id",
-      "non_preferred_brand_drugs",
-      "preferred_brand_drugs",
-      "specialty_drugs",
-      "nonpreferred_generic_drug_share",
-      "nonpreferred_specialty_drug_share"
-    )
+  def format_plan(plan, tier)
+    {
+      id: plan['id'],
+      name: "#{plan['carrier_name']}: #{plan['display_name']}",
+      medal: plan['level'],
+      ec: drug_covered(tier),
+      cost: drug_cost(plan, tier),
+      pre: plan['premium'] || 'contact for rates'
+    }
+  end
+
+  def drug_covered(tier)
+    case tier
+    when "preferred_brand", "non_preferred_brand"
+      "covered"
+    when "not_covered"
+      "not covered"
+    else
+      "not listed"
+    end
+  end
+
+  def drug_cost(plan, tier)
+    case tier
+    when "preferred_brand", "non_preferred_brand"
+      plan["#{tier}_drugs"]
+    else
+      "-"
+    end
   end
 
   # dynamically searches for drug ids
