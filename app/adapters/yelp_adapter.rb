83e475b2c8915710e9549030bea216d73129faf8
nikymorg <nicole.morgan@gmail.com>
2018-10-20 16:40:00 -0400

    format pharmacy data

--- app/adapters/yelp_adapter.rb
+++ app/adapters/yelp_adapter.rb
@@ -15,6 +15,27 @@ class YelpAdapter
     }
 
     resp = RestClient.get(base_url, {Authorization: headers, params: params})
-    JSON.parse(resp)['businesses']
+    pharmacies = JSON.parse(resp)['businesses']
+    pharmacies.map{|pharmacy| format_pharmacy(pharmacy)}
   end
+
+  def format_pharmacy(pharmacy)
+    {
+      id: pharmacy['id'],
+      name: pharmacy['name'],
+      address: pharmacy['location']['address1'],
+      city: pharmacy['location']['city'],
+      state: pharmacy['location']['state'],
+      zipcode: pharmacy['location']['zip_code'],
+      recommended: [true, false].sample,
+      numberOfPharmacists: rand(1..5),
+      otc: [true, false].sample,
+      latitude: pharmacy['coordinates']['latitude'],
+      longitude: pharmacy['coordinates']['longitude']
+    }
+  end
+
+
+
+
 end
