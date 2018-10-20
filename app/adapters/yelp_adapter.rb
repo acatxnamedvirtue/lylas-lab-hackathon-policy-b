a37c1694e265a382678815131131d05d618e453a
nikymorg <nicole.morgan@gmail.com>
2018-10-20 15:46:19 -0400

    yelp adapter

---
+++ app/adapters/yelp_adapter.rb
@@ -0,0 +1,20 @@
+class YelpAdapter
+  attr_reader :headers, :base_url
+
+  def initialize
+    @base_url = 'https://api.yelp.com/v3/businesses/search'
+    @headers = "Bearer #{ENV['yelp_api_key']}"
+  end
+
+
+  def get_pharmacies(zip_code)
+    params = {
+      term: 'pharmacy',
+      location: zip_code,
+      limit: 10
+    }
+
+    resp = RestClient.get(base_url, {Authorization: headers, params: params})
+    JSON.parse(resp)['businesses']
+  end
+end
--- app/controllers/api/v1/pharmacies_controller.rb
+++ app/controllers/api/v1/pharmacies_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::PharmaciesController < ApplicationController
   before_action :set_zip_code, only: [:index]
 
   def index
-    client = PharmacyAdapter.new
+    client = YelpAdapter.new
     pharmacies = client.get_pharmacies(@zip_code)
     render json: pharmacies
   end
